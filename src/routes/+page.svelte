baf720339695875e4069f72ddf20b513a00aedef
Jimmy <risenalpha@gmail.com>
2024-12-17 10:30:27 -0700

    accessibility

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,11 @@
   import { About, Education, Experience, Hero, Skills } from '$lib/components/sections';
 </script>
 
+<svelte:head>
+  <!-- jimmy truong (dot) Home-->
+  <title>Jimmy Truong • Home</title>
+</svelte:head>
+
 <div class="flex w-full flex-col gap-y-10">
   <Hero />
   <About />
